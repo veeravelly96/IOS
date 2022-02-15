f682023ad4bbfd87a5b8bd51e0499e0c606a9eff
sai krishna veeravelly <S544724@NWMISSOURI.EDU>
2022-02-14 18:19:47 -0600

    Add files via upload

---
+++ veeravelly_Calculator/veeravelly_Calculator/ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  veeravelly_Calculator
+//
+//  Created by Veeravelly,Sai Krishna on 2/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var Displaylabel: UILabel!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func ButtonAC(_ sender: UIButton) {
+    }
+    
+    @IBAction func ButtonC(_ sender: UIButton) {
+    }
+    
+    @IBAction func ButtonChangeSign(_ sender: UIButton) {
+    }
+    
+    @IBAction func Buttondivison(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button7(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button8(_ sender: UIButton) {
+    }
+    @IBAction func Button9(_ sender: UIButton) {
+    }
+    
+    @IBAction func ButtonX(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button4(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button5(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button6(_ sender: UIButton) {
+    }
+    
+    @IBAction func Buttonminus(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button1(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button2(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button3(_ sender: UIButton) {
+    }
+    
+    @IBAction func Buttonplus(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button0(_ sender: UIButton) {
+    }
+    
+    @IBAction func Buttondot(_ sender: UIButton) {
+    }
+    
+    @IBAction func Buttonpercentile(_ sender: UIButton) {
+    }
+    @IBAction func Buttonequals(_ sender: UIButton) {
+    }
+    
+    
+}
+
