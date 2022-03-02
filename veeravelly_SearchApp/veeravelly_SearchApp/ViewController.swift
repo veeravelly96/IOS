0fcadb636953a9d12bf5fd1c65fd3b557ca6ec4e
sai krishna veeravelly <S544724@NWMISSOURI.EDU>
2022-03-02 16:00:42 -0600

    Assignment4

---
+++ veeravelly_SearchApp/veeravelly_SearchApp/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  veeravelly_SearchApp
+//
+//  Created by Veeravelly,Sai Krishna on 3/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    
+    @IBOutlet weak var resultImage: UIImageView!
+    
+    @IBOutlet weak var topicInfoText: UITextView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func searchButtonAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func showNextImagesBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func ShowPrevImagesBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func ResetButtonClicked(_ sender: UIButton) {
+    }
+    
+}
+
